bdfe8887346a833a45aaf547c452be5d9fabd3da
Swaroop Ramaswamy <ramaswamy.swaroop@gmail.com>
2015-04-27 13:59:49 -0700

    Changed Kruskal's implementation into a function

--- KruskalSingleMachine.scala
+++ KruskalSingleMachine.scala
@@ -1,33 +1,43 @@
 import util._
 
-val V = Array(1, 2, 3, 4, 5, 6, 7)
-val E = Array(((1,4),5), ((3,5),5), ((4,6),6), ((1,2),7), ((2,5),7), ((2,3),8), ((5,6),8), ((2,4),9), ((5,7),9), ((6,7),11), ((4,5),15))
+def Kruskal(V : Array[Int], E : Array[((Int,Int), Int)]): Array[((Int,Int), Int)] = {
 
-var tree = new Array[((Int, Int), Int)](0)
+    //val V = Array(1, 2, 3, 4, 5, 6, 7)
+    //val E = Array(((1,4),5), ((3,5),5), ((4,6),6), ((1,2),7), ((2,5),7), ((2,3),8), ((5,6),8), ((2,4),9), ((5,7),9), ((6,7),11), ((4,5),15))
 
-var x = new DisjointSet[Int]
+    //val V = Array(1,2,3,4,5,6)
+    //val E = Array(((1,2),3), ((1,3),5), ((2,3),2), ((4,5),3), ((4,6),5), ((5,6),2))
 
-for(v <- V)
-	x.add(v)
+    val Esorted = E.sortBy(_._2)
+
+    var tree = new Array[((Int, Int), Int)](0)
+
+    var x = new DisjointSet[Int]
+
+    for(v <- V)
+        x.add(v)
 
 
-for(e <- E)
-{
-	val u = e._1._1
-	val v = e._1._2
+        for(e <- Esorted)
+        {
+            val u = e._1._1
+            val v = e._1._2
 
-	if(x.find(u) != x.find(v))
-	{
-		tree = tree :+ e
-		x.union(u,v)
-	}
-}
+            if(x.find(u) != x.find(v))
+            {
+                tree = tree :+ e
+                x.union(u,v)
+            }
+        }
 
-for(e <- tree)
-{
-	val u = e._1._1
-	val v = e._1._2
-	val w = e._2
+        for(e <- tree)
+        {
+            val u = e._1._1
+            val v = e._1._2
+            val w = e._2
 
-	println("{(" + u + ", " + v + "), " + w + "}")
+            println("{(" + u + ", " + v + "), " + w + "}")
+        }
+
+        return tree
 }
