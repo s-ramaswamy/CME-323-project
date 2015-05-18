e156063bb45ff227c6f8db8551f94715dfdac381
rohitpatki <rohitpatki.iitm@gmail.com>
2015-05-17 18:46:08 -0700

    updated partition file

--- partition.scala
+++ partition.scala
@@ -19,22 +19,19 @@ def partition(edge: ((Int,Int),Int), v: Array[(Int,Int)], n: Int): Array [((Int,
 		return A
 	}
 	else{
-		var a = 0
-		var small = 0
-		var big = 0
-		//var T = new Array[((Int,Int),((Int,Int),Int))](n)
-		var vlist = 1 to n toArray
-		for(a <- vlist){
-			small = min(v(edge._1._1 - 1)._2,a-1)
-			big = max(v(edge._1._1 - 1)._2,a-1)
-			yield((small,big),edge)
-			//T(a-1) = ((small,big),edge)
+		var a = 0;
+		var small = 0;
+		var big = 0;
+		var vlist = 1 to n toArray;
+		for(a <- vlist) yield {
+			small = min(v(edge._1._1 - 1)._2,a-1);
+			big = max(v(edge._1._1 - 1)._2,a-1);
+			((small,big),edge);
 		}
-		return T
 	}
 }
 
-val temp = e.flatMap(x => partition(x,V1))
+val temp = e.flatMap(x => partition(x,Vpart,3))
 val t = sc.parallelize(temp)
 val edge_partition = t.groupByKey.collect()
 
