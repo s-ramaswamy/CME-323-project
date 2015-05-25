52c7ef578fdf5e4bf697a464f6ffdd54ddb02dff
Swaroop Ramaswamy <ramaswamy.swaroop@gmail.com>
2015-05-25 16:18:34 -0700

    Modified algorithm3.scala

--- algorithm3.scala
+++ algorithm3.scala
@@ -17,7 +17,7 @@ def mapfunction(x: Iterable[((Int,Int),Int)]): Array[((Int,Int), Int)] = {
     return y
 }
 
-def findminimum(elist: Array[((Int,Int),Int)], xbroad: scala.collection.immutable.Map[Int,Int]): Map[Int,((Int,Int),Int)] = {
+def findminimum(elist: Array[((Int,Int),Int)], xbroad: scala.collection.immutable.HashMap[Int,Int]): Map[Int,((Int,Int),Int)] = {
     var y:Map[Int,((Int,Int),Int)]  = Map()
     for(e <- elist)
     {
@@ -36,7 +36,7 @@ def findminimum(elist: Array[((Int,Int),Int)], xbroad: scala.collection.immutabl
 
             if(y(compU)._2 > wt)
                 y(compU) = e
-
+                        
             if(y(compV)._2 > wt)
                 y(compV) = e
         }
@@ -47,8 +47,7 @@ def findminimum(elist: Array[((Int,Int),Int)], xbroad: scala.collection.immutabl
 def minfunction(x: ((Int,Int), Int), y: ((Int,Int), Int)): ((Int, Int), Int) = {
     if(x._2 > y._2 && y._2 != -1)
         return y
-    else
-        return x
+    return x
 }    
 
 def reduceMaps(p: Map[Int,((Int,Int),Int)], q: Map[Int,((Int,Int),Int)]): Map[Int,((Int,Int),Int)] = {
@@ -60,8 +59,8 @@ def reduceMaps(p: Map[Int,((Int,Int),Int)], q: Map[Int,((Int,Int),Int)]): Map[In
 //val E = Array(((1,4),5), ((3,5),5), ((4,6),6), ((1,2),7), ((2,5),7), ((2,3),8), ((5,6),8), ((2,4),9), ((5,7),9), ((6,7),11), ((4,5),15))
 
 //val distE = sc.parallelize(E)
-val V = 0 to 2088092  toArray
-val distK = sc.textFile("roadnet.txt")
+val V = 0 to 1696415  toArray
+val distK = sc.textFile("as-skitter.txt")
 val distE = distK.map(s => ((s.split("\\s+")(0).toInt,s.split("\\s+")(1).toInt),(1).toInt))
 var A = new DisjointSet[Int]
 for(i <- V){
@@ -73,10 +72,11 @@ val niter =(math.log(V.size)/math.log(2)).ceil toInt
 println(niter)
 var edgelistsize = 1
 for(m <- 1 to niter if edgelistsize != 0){
-        var B:scala.collection.immutable.Map[Int,Int] = scala.collection.immutable.Map()
+        var B:scala.collection.immutable.HashMap[Int,Int] = scala.collection.immutable.HashMap()
         for(i <- V){
             B += (i -> A.find(i))
         }
+
         var xbroad = sc.broadcast(B)
         var edgelist = distEgrouped.map(a => findminimum(mapfunction(a._2), xbroad.value)).reduce((a,b) => reduceMaps(a,b))
         edgelistsize = edgelist.size
